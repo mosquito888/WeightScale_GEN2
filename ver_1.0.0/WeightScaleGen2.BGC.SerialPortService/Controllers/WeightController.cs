c0c362b4a50708850567563fde8799b81bda83ae
phonlayut17 <47514597+mosquito888@users.noreply.github.com>
2025-09-16 13:18:35 +0700

    [Updated] : Code

---
+++ ver_1.0.0/WeightScaleGen2.BGC.SerialPortService/Controllers/WeightController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WeightScaleGen2.BGC.SerialPortService.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class WeightController : ApiController
+    {
+        [HttpGet]
+        [Route("api/runservice")]
+        public IHttpActionResult RunService()
+        {
+            return Ok("OK");
+        }
+
+        [HttpGet]
+        [Route("api/weight")]
+        public IHttpActionResult GetWeight()
+        {
+            return Ok(Program.latestWeight);
+        }
+    }
+}
